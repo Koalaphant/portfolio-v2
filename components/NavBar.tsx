a632da712422f235195010c120e36687891652e3
Koalaphant <andrew.wardjones@icloud.com>
2024-08-08 11:43:23 +0100

    adding response navbar menu icons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,67 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
+const navLinks = [
+  {
+    title: "About",
+    href: "#about",
+  },
+  {
+    title: "Projects",
+    href: "#projects",
+  },
+  {
+    title: "Contact",
+    href: "#contact",
+  },
+];
 
 export const NavBar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
+
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link className="text-white font-semibold text-5xl" href={"/"}>Logo</Link>
-        <div className="menu"></div>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2 md:py-4">
+        <Link
+          className="text-2xl md:text-5xl text-white font-semibold "
+          href={"/"}
+        >
+          Logo
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navBarOpen ? (
+            <button
+              onClick={() => setNavBarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavBarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul>
+            <li>
+              <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                {navLinks.map((link, index) => (
+                  <li key={index}>
+                    <NavLink href={link.href} title={link.title} />
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
